33e046ea4fcfd5989f228a083f51321bf8d4613c
iCoder38 <dishant.rajput@evirtualservices.com>
2023-10-25 13:52:33 +0530

    update : check dev

--- lib/friends/friends.dart
+++ lib/friends/friends.dart
@@ -132,6 +132,9 @@ class _FriendsScreenState extends State<FriendsScreen> {
                   GestureDetector(
                     onTap: () {
                       //
+                      // sasa
+                      // new Work(
+                      // ),
                       Navigator.push(
                         context,
                         MaterialPageRoute(
